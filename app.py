5ccd76b4988a720e86998677f6c96c14d9bbb6e4
Steve-Jovs <69207333+Steve-Jovs@users.noreply.github.com>
2024-04-12 19:02:06 +0800

    Can post to Mastodon using Flask webapp

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 import requests
 
 #change based on saved directory
@@ -7,15 +7,24 @@ path = r'D:\UPD\cs173\SkyMast'
 app = Flask(__name__, static_folder = path)
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET', 'DELETE'])
 def home():
-    return render_template("index.html")
+    if request.method == 'POST':
+        post_text = request.form['post-text']
+
+        try:
+            url = 'https://mastodon.social/api/v1/statuses'
+            auth = {'Authorization': 'Bearer zfBQR1uwx1iQndjCuZ5H-JPCCZpklrHLp1br6FfeKS4'}
+            params = {'status': post_text}
+            response = requests.post(url, data=params, headers=auth)
+            print(response, "Gumana siya")
+            return redirect('/')
+
+        except:
+            return 'There was an issue adding your task'
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
 
-url = 'https://mastodon.social/api/v1/statuses'
-auth = {'Authorization': 'Bearer zfBQR1uwx1iQndjCuZ5H-JPCCZpklrHLp1br6FfeKS4'}
-params = {'status': 'Test 2: How do i recreate that test 1'}
-response = requests.post(url, data=params, headers=auth)
-print(response, "Gumana siya")
