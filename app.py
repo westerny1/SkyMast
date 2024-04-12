955ef98b6d4540b9ada9684acaef172fd4d90e85
Steve-Jovs <69207333+Steve-Jovs@users.noreply.github.com>
2024-04-12 18:09:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, render_template
+import requests
 
 #change based on saved directory
 path = r'D:\UPD\cs173\SkyMast'
@@ -12,3 +13,9 @@ def home():
 
 if __name__ == "__main__":
     app.run()
+
+url = 'https://mastodon.social/api/v1/statuses'
+auth = {'Authorization': 'Bearer zfBQR1uwx1iQndjCuZ5H-JPCCZpklrHLp1br6FfeKS4'}
+params = {'status': 'Test 2: How do i recreate that test 1'}
+response = requests.post(url, data=params, headers=auth)
+print(response, "Gumana siya")
